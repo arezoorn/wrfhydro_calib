e2ff1ebf7d3ca30cfd977c69f28d1e3d4fe62dd4
arezoorn <arezoo.rafieeinasab@gmail.com>
2018-01-08 09:41:28 -0700

    changing the SA scheme to DELSA

--- CalibDemo/sens_workflow_pre.R
+++ CalibDemo/sens_workflow_pre.R
@@ -1,8 +1,10 @@
-.libPaths("/glade/u/home/adugger/system/R/Libraries/R3.2.2")
+
 library(rwrfhydro)
 library(data.table)
 library(ggplot2)
 library(plyr)
+library(sensitivity)
+library(randtoolbox)
 
 #########################################################
 # SETUP
@@ -24,43 +26,38 @@ cyclecount <- 0
 
 # Setup value lists from paramBnds
 message("Setup lists")
-xnames <- paramBnds$param
-x0 <- paramBnds$ini
-names(x0) <- xnames
-x_min <- paramBnds$min
-names(x_min) <- xnames
-x_max <- paramBnds$max
-names(x_max) <- xnames
 
-# Setup parameter sets
-message("Setup parameter sets")
-x_all <- data.frame(t(x0))
-x_all$tag <- "control"
-x_all$bound <- "control"
-cnt <- 2
-for (ivar in names(x0)) {
-      message(ivar)
-      x_all[cnt,] <- c(as.numeric(x0), "", "")
-      x_all[cnt,ivar] <- as.numeric(paramBnds$min[paramBnds$param==ivar])
-      x_all[cnt,"tag"] <- ivar
-      x_all[cnt,"bound"] <- "Low"
-      x_all[cnt+1,] <- c(x0, "", "")
-      x_all[cnt+1,ivar] <- as.numeric(paramBnds$max[paramBnds$param==ivar])
-      x_all[cnt+1,"tag"] <- ivar
-      x_all[cnt+1,"bound"] <- "High"
-      cnt <- cnt+2
+if (SA_method == "DELSA") {
+  
+  # Setup value lists from paramBnds
+  par.ranges <- list()
+  for(i in 1:nrow(paramBnds)) {
+    par.ranges[[paramBnds[i, "param"]]] <- unname(unlist(paramBnds[i, c("min", "max")]))
+  }
+  X0 <- sensitivity::parameterSets(par.ranges, samples = SA_sample_size, method = c(SA_par_gen_method))
+  varprior = sapply(par.ranges, diff)^2/12
+  
+  X = do.call(rbind, lapply(1:ncol(X0), function(i) {
+    X2i = X0
+    X2i[, i] = X2i[, i] * SA_perturb
+    X2i
+  }))
+  
+  x_all = as.data.frame(rbind(X0, X))
+  names(x_all) <- names(par.ranges)
+  
 }
-for (i in xnames) { x_all[,i] <- as.numeric(x_all[,i]) }
+# add the index as the first columns, since it existed in Aubrey s way of coding 
+x_all <- cbind.data.frame(id = c(1:nrow(x_all)), x_all)
 
 # Initialize parameter archive DF
 message("Initialize parameter archive")
-x_archive <- as.data.frame(matrix(, nrow=1, ncol=length(xnames)+11))
-names(x_archive) <- c("id", xnames, "tag", "bound", metrics)
+x_archive <- as.data.frame(matrix(, nrow=1, ncol=ncol(x_all)+length(metrics)))
+names(x_archive) <- c(names(x_all), metrics)
 
 # Output parameter set
 message("Output parameter set")
-x_all_out <- cbind(data.frame(id=seq(1:nrow(x_all))), x_all[,1:14])
-write.table(x_all_out, file="params_new.txt", row.names=FALSE, sep=" ")
+write.table(x_all, file="params_new.txt", row.names=FALSE, sep=" ")
 
 # Save and exit
 save.image("proj_data_SENS.Rdata")
