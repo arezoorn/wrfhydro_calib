124a1a37532f5d717f4e18f63acc4b0f8df13457
arezoorn <arezoo@ucar.edu>
2018-01-22 13:29:04 -0700

    Complying with V1.2 calib workflow
    I made some changes to make it compatible with the V1.2 calibration workflow that Logan has. So it would be easier to make the workflow.

--- CalibDemo/sens_workflow_pre.R
+++ CalibDemo/sens_workflow_pre.R
@@ -1,3 +1,6 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly=TRUE)
+namelistFile <- args[1]
 
 library(rwrfhydro)
 library(data.table)
@@ -11,28 +14,32 @@ library(randtoolbox)
 #########################################################
 
 source("calib_utils.R")
+source(namelistFile)
+objFunc <- get(objFn)
 
 # Metrics
-metrics <- c("obj", "nse", "nselog", "cor", "rmse", "bias", "kge", "fdc")
-
+metrics <- c("cor", "rmse", "bias", "nse", "nselog", "nsewt", "kge", "msof")
 
 #########################################################
 # MAIN CODE
 #########################################################
 
 # First run so need to initialize
-ReadNamelist("namelist.calib")
 cyclecount <- 0
 
 # Setup value lists from paramBnds
 message("Setup lists")
+
+# Read parameter bounds 
+paramBnds <- read.table(paste0(runDir, "/sens_params.tbl"), header=TRUE, sep=",", stringsAsFactors=FALSE)
+paramBnds <- subset(paramBnds, paramBnds$sens_flag==1)
 
 if (SA_method == "DELSA") {
   
   # Setup value lists from paramBnds
   par.ranges <- list()
   for(i in 1:nrow(paramBnds)) {
-    par.ranges[[paramBnds[i, "param"]]] <- unname(unlist(paramBnds[i, c("min", "max")]))
+    par.ranges[[paramBnds[i, "parameter"]]] <- unname(unlist(paramBnds[i, c("minValue", "maxValue")]))
   }
   X0 <- sensitivity::parameterSets(par.ranges, samples = SA_sample_size, method = c(SA_par_gen_method))
   varprior = sapply(par.ranges, diff)^2/12
@@ -46,7 +53,10 @@ if (SA_method == "DELSA") {
   x_all = as.data.frame(rbind(X0, X))
   names(x_all) <- names(par.ranges)
   
+} else {
+  stop("It only supports the DELSA technique at this point")
 }
+
 # add the index as the first columns, since it existed in Aubrey s way of coding 
 x_all <- cbind.data.frame(id = c(1:nrow(x_all)), x_all)
 
@@ -55,13 +65,13 @@ message("Initialize parameter archive")
 x_archive <- as.data.frame(matrix(, nrow=1, ncol=ncol(x_all)+length(metrics)))
 names(x_archive) <- c(names(x_all), metrics)
 
-x_archive_h <- x_archive
+x_archive_h <- x_archive # adding this since we want to calculate both at daily and hourly time step
 
 # Output parameter set
 message("Output parameter set")
-write.table(x_all, file="params_new.txt", row.names=FALSE, sep=" ")
+write.table(x_all, file=paste0(runDir, "/params_new.txt"), row.names=FALSE, sep=" ")
 
 # Save and exit
-save.image("proj_data_SENS.Rdata")
+save.image(paste0(runDir, "/proj_data_SENS.Rdata"))
 quit("no")
 
